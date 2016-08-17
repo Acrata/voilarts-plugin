5efe5359535f77a1748ab5f8314c4669b3d1e755
vit <vit@canaima-popular.(none)>
2016-08-17 13:27:16 -0430

    initial commit

---
+++ parts/settings/slides.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Title: Text Fields
+Order: 10
+Tab: Common
+Sub Tab: Basic
+Setting: voilarts_settings
+Flow: Demo Workflow
+*/
+?>
+
+<div class="piklist-demo-highlight">
+  <?php _e('Text fields are at the core of most forms, and easily created with Piklist. Tooltip help can be added to any field with one line of code.', 'piklist-demo');?>
+</div>
+
+<h3>Mis sets plug</h3>
+<?php
+
+  piklist('field', array(
+    'type' => 'group'
+    ,'field' => 'slides'
+    ,'add_more' => true
+    ,'label' => __('Slide Images', 'voilarts')
+    ,'description' => __('Add the slides for the slideshow.  You can add as many slides as you want, and they can be drag-and-dropped into the order that you would like them to appear.', 'voilarts')
+    ,'fields'  => array(
+      array(
+        'type' => 'file'
+        ,'field' => 'image'
+        ,'label' => __('Slides', 'voilarts')
+        ,'columns' => 12
+        ,'validate' => array(
+          array(
+            'type' => 'limit'
+            ,'options' => array(
+              'min' => 1
+              ,'max' => 1
+            )
+          )
+        )
+      )
+      ,array(
+        'type' => 'text'
+        ,'field' => 'url'
+        ,'label' => __('URL', 'voilarts')
+        ,'columns' => 12
+      )
+      ,array(
+        'type' => 'text'
+        ,'field' => 'caption'
+        ,'label' => __('Caption', 'voilarts')
+        ,'columns' => 12
+      )
+    )
+  ));
+
+  piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'text'
+    ,'label' => __('Text', 'voilarts')
+    ,'help' => __('You can easily add tooltips to your fields with the help parameter.', 'voilarts')
+    ,'attributes' => array(
+      'class' => 'regular-text'
+    )
+  ));
+
+  piklist('field', array(
+    'type' => 'textarea'
+    ,'field' => 'demo_textarea_large'
+    ,'label' => __('Area de texto', 'voilarts')
+    ,'description' => 'class="large-text code" rows="10" columns="50"'
+    ,'value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    ,'attributes' => array(
+      'rows' => 10
+      ,'cols' => 50
+      ,'class' => 'large-text'
+    )
+  ));
+
+  piklist('shared/code-locater', array(
+    'location' => __FILE__
+    ,'type' => 'Settings Section'
+  ));
---
+++ voilarts.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Plugin Name: Voilarts
+Plugin URI:
+Description: Street and circus show
+Version: 1.0
+Author: Hector Ovalles
+Author URI:
+Plugin Type: Piklist
+License: GNU
+Text Domain: voilarts
+*/
+
+
+    // Localisation Support
+    //load_theme_textdomain('voilarts', get_template_directory() . '/languages');
+
+
+/**
+   * Register a book post type, with REST API support
+   *
+   * Based on example at: http://codex.wordpress.org/Function_Reference/register_post_type
+   */
+  add_action( 'init', 'my_artist_cpt' );
+  function my_artist_cpt() {
+  	$labels = array(
+  		'name'               => _x( 'Artists', 'post type general name', 'voilarts' ),
+  		'singular_name'      => _x( 'Artist', 'post type singular name', 'voilarts' ),
+  		'menu_name'          => _x( 'Artist', 'admin menu', 'voilarts' ),
+  		'name_admin_bar'     => _x( 'Artist', 'add new on admin bar', 'voilarts' ),
+  		'add_new'            => _x( 'Add New', 'artist', 'voilarts' ),
+  		'add_new_item'       => __( 'Add New Book', 'voilarts' ),
+  		'new_item'           => __( 'New Artist', 'voilarts' ),
+  		'edit_item'          => __( 'Edit Artist', 'voilarts' ),
+  		'view_item'          => __( 'View Artist', 'voilarts' ),
+  		'all_items'          => __( 'All Artist', 'voilarts' ),
+  		'search_items'       => __( 'Search Artists', 'voilarts' ),
+  		'parent_item_colon'  => __( 'Parent Artists:', 'voilarts' ),
+  		'not_found'          => __( 'No artists found.', 'voilarts' ),
+  		'not_found_in_trash' => __( 'No artists found in Trash.', 'voilarts' )
+  	);
+
+  	$args = array(
+  		'labels'             => $labels,
+  		'description'        => __( 'Description.', 'voilarts' ),
+  		'public'             => true,
+  		'publicly_queryable' => true,
+  		'show_ui'            => true,
+  		'show_in_menu'       => true,
+  		'query_var'          => true,
+  		'rewrite'            => array( 'slug' => 'artist' ),
+  		'capability_type'    => 'post',
+  		'has_archive'        => true,
+  		'hierarchical'       => false,
+  		'menu_position'      => null,
+  		'show_in_rest'       => true,
+  		'rest_base'          => 'artists-api',
+  		'rest_controller_class' => 'WP_REST_Posts_Controller',
+  		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+  	);
+
+  	register_post_type( 'artist', $args );
+}
+
+
+add_filter('piklist_admin_pages', 'artist_ex');
+  function artist_ex($pages)
+  {
+     $pages[] = array(
+      'page_title' => __('')
+      ,'menu_title' => __('Slider', 'artist')
+      ,'sub_menu' => 'edit.php?post_type=artist'
+      ,'capability' => 'manage_options'
+      ,'menu_slug' => 'custom_settings'
+      ,'setting' => 'voilarts_settings'
+      ,'menu_icon' => plugins_url('piklist/parts/img/piklist-icon.png')
+      ,'page_icon' => plugins_url('piklist/parts/img/piklist-page-icon-32.png')
+      ,'single_line' => true
+      ,'default_tab' => 'Basic'
+      ,'save_text' => 'Save Demo Settings'
+    );
+
+    return $pages;
+  }
