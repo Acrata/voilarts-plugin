32a043babb65fa7a4b32aac8d1df50a6cac00118
vit <vit@canaima-popular.(none)>
2016-08-18 18:14:03 -0430

    rest,api,vue

--- voilarts.php
+++ voilarts.php
@@ -83,7 +83,7 @@ add_filter('piklist_admin_pages', 'artist_ex');
   }
 
 //wp_enqueue_script('bxSlider');
-add_action('wp_enqueue_script','register_my_scripts');
+add_action('init','register_my_scripts');
 
 function register_my_scripts(){
 wp_register_script('bxSlider', plugin_dir_url(__FILE__) . 'bower_components/bxslider-4/dist/jquery.bxslider.js', array('jquery'));
