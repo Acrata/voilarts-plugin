1e3df0b3ee422a04beceed90c1729349b81121cf
Acrata <hxtr227@gmail.com>
2016-12-08 17:26:57 -0430

    brand new site again

--- voilarts.php
+++ voilarts.php
@@ -28,7 +28,7 @@ Text Domain: voilarts
   		'menu_name'          => _x( 'Artist', 'admin menu', 'voilarts' ),
   		'name_admin_bar'     => _x( 'Artist', 'add new on admin bar', 'voilarts' ),
   		'add_new'            => _x( 'Add New', 'artist', 'voilarts' ),
-  		'add_new_item'       => __( 'Add New Book', 'voilarts' ),
+  		'add_new_item'       => __( 'Add New Artista', 'voilarts' ),
   		'new_item'           => __( 'New Artist', 'voilarts' ),
   		'edit_item'          => __( 'Edit Artist', 'voilarts' ),
   		'view_item'          => __( 'View Artist', 'voilarts' ),
@@ -50,19 +50,80 @@ Text Domain: voilarts
   		'rewrite'            => array( 'slug' => 'artist' ),
   		'capability_type'    => 'post',
   		'has_archive'        => true,
-  		'hierarchical'       => false,
+        'hierarchical'       => true,
   		'menu_position'      => null,
+        'edit_columns' => array(
+          'title' => 'Demo',
+          'author' => 'Assigned to'
+            ),
   		'show_in_rest'       => true,
   		'rest_base'          => 'artists-api',
   		'rest_controller_class' => 'WP_REST_Posts_Controller',
-  		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+  		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'page-attributes' ),
+        'taxonomies' => array(
+            ''
+        ) // Add Category and Post Tags support
   	);
 
   	register_post_type( 'artist', $args );
 }
+  //add_filter('piklist_post_types', 'artist_cpt_pk');
+  function artist_cpt_pk($post_types)
+  {
+  	$labels_art = array(
+  		'name'               => _x( 'Artists', 'post type general name', 'voilarts' ),
+  		'singular_name'      => _x( 'Artist', 'post type singular name', 'voilarts' ),
+  		'menu_name'          => _x( 'Artist', 'admin menu', 'voilarts' ),
+  		'name_admin_bar'     => _x( 'Artist', 'add new on admin bar', 'voilarts' ),
+  		'add_new'            => _x( 'Add New', 'artist', 'voilarts' ),
+  		'add_new_item'       => __( 'Add New Artista', 'voilarts' ),
+  		'new_item'           => __( 'New Artist', 'voilarts' ),
+  		'edit_item'          => __( 'Edit Artist', 'voilarts' ),
+  		'view_item'          => __( 'View Artist', 'voilarts' ),
+  		'all_items'          => __( 'All Artist', 'voilarts' ),
+  		'search_items'       => __( 'Search Artists', 'voilarts' ),
+  		'parent_item_colon'  => __( 'Parent Artists:', 'voilarts' ),
+  		'not_found'          => __( 'No artists found.', 'voilarts' ),
+  		'not_found_in_trash' => __( 'No artists found in Trash.', 'voilarts' )
+  	);
+    $post_types['artist'] = array(
+      //'labels' => $labels_art
+      //,'title' => __('Enter a new Artist')
+      //,'menu_icon' => piklist('url', 'piklist') . '/parts/img/piklist-menu-icon.svg'
+      //,'page_icon' => piklist('url', 'piklist') . '/parts/img/piklist-page-icon-32.png'
+      'edit_columns' => array(
+      )
+      //,'hide_meta_box' => array(
+        //'author'
+      //)
+      //,'status' => array(
+        //'new' => array(
+          //'label' => 'New'
+          //,'public' => false
+        //)
+        //,'pending' => array(
+          //'label' => 'Pending Review'
+          //,'public' => false
+        //)
+        //,'demo' => array(
+          //'label' => 'Demo'
+          //,'public' => true
+          //,'exclude_from_search' => true
+          //,'show_in_admin_all_list' => true
+          //,'show_in_admin_status_list' => true
+       //)
+        //,'lock' => array(
+          //'label' => 'Lock'
+          //,'public' => true
+        //)
+      //)
+    );
+    return $post_types;
+  }
 
 
-add_filter('piklist_admin_pages', 'artist_ex');
+
+//add_filter('piklist_admin_pages', 'artist_ex');
   function artist_ex($pages)
   {
      $pages[] = array(
